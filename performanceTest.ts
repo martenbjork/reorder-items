55189d31ceb3c42c5d3aca016c9976a8102cbb12
Marten Bjork <marten@martenbjork.com>
2021-03-12 21:48:36 -0500

    Test different types of actions

--- performanceTest.ts
+++ performanceTest.ts
@@ -1,45 +1,64 @@
 import { reorder } from "./src/reorder";
-import { SortableItem } from "./src/reorder.types";
+import { Action, SortableItem } from "./src/reorder.types";
 
 function measurePerformance(runs: number, columns: number, rows: number) {
   let grid: SortableItem[] = [];
 
-  for (var column = 0; column < columns; column++) {
-    for (var order = 0; order < rows; order++) {
-      grid.push({
-        id: `${column}-${order}`,
-        column,
-        order,
-      });
-    }
-  }
-
-  const testId = `${runs} MOVE operations on a ${columns} x ${rows} grid`;
-
-  console.time(testId);
-  for (var i = 0; i < runs; i++) {
-    reorder(grid, {
+  const actions: Action[] = [
+    {
       type: "MOVE",
       id: "0-0",
       toColumn: 1,
-      toOrder: 1,
-    });
-  }
-  console.timeEnd(testId);
+      toOrder: 0,
+    },
+    {
+      type: "INSERT",
+      item: {
+        id: "Q",
+      },
+      order: 0,
+      column: 0,
+    },
+    {
+      type: "REMOVE",
+      id: "0-0",
+    },
+  ];
+
+  actions.forEach((action) => {
+    for (var column = 0; column < columns; column++) {
+      for (var order = 0; order < rows; order++) {
+        grid.push({
+          id: `${column}-${order}`,
+          column,
+          order,
+        });
+      }
+    }
+
+    const testId = `${runs} ${action.type} operations on a ${columns} x ${rows} grid`;
+
+    console.time(testId);
+    for (var i = 0; i < runs; i++) {
+      reorder(grid, action);
+    }
+    console.timeEnd(testId);
+  });
+  console.log("\n");
 }
 
 // Increasing number of runs
-measurePerformance(10, 100, 100);
-measurePerformance(100, 100, 100);
+measurePerformance(1, 100, 100);
+measurePerformance(1, 100, 100);
 // measurePerformance(1000, 100, 100);
 
 // Increasing scale of grid
-measurePerformance(100, 10, 100);
-measurePerformance(100, 100, 100);
-measurePerformance(100, 1000, 100);
-measurePerformance(100, 100, 10);
-measurePerformance(100, 100, 100);
-measurePerformance(100, 100, 1000);
+measurePerformance(1, 10, 100);
+measurePerformance(1, 100, 100);
+measurePerformance(1, 1000, 100);
+measurePerformance(1, 100, 10);
+measurePerformance(1, 100, 100);
+measurePerformance(1, 100, 1000);
 
 // Realistic worst case scenario
 measurePerformance(1, 250, 20);
